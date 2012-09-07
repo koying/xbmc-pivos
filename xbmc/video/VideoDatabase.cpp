ab04932ccae22ba80b9929ee8ae3fb1edce0d31a
Chris "Koying" Browet <cbro@semperpax.com>
2012-09-07 16:01:28 +0200

    ADD: add advanced settings allowing to customize the items of the "Recently Added" job
    Conflicts:
    
    	xbmc/utils/RecentlyAddedJob.cpp

--- xbmc/video/VideoDatabase.cpp
+++ xbmc/video/VideoDatabase.cpp
@@ -4949,7 +4949,6 @@ bool CVideoDatabase::GetSetsByWhere(const CStdString& strBaseDir, const Filter &
     if (!setFilter.order.empty())
       setFilter.order += ",";
     setFilter.order += "sets.idSet";
-
     if (!GetMoviesByWhere(strBaseDir, setFilter, items))
       return false;
 
