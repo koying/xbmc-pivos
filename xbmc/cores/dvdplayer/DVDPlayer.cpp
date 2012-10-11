a56a66fa64e363901d50e43aee8c49980165fc3e
davilla <davilla@4pi.com>
2012-10-11 11:12:57 -0400

    [aml] add DVDVideoCodecAmlogic

--- xbmc/cores/dvdplayer/DVDPlayer.cpp
+++ xbmc/cores/dvdplayer/DVDPlayer.cpp
@@ -2862,7 +2862,9 @@ bool CDVDPlayer::OpenVideoStream(int iStream, int source, bool reset)
       hint.aspect = aspect;
       hint.forced_aspect = true;
     }
+#if !defined(HAS_LIBAMCODEC)
     hint.software = true;
+#endif
   }
 
   boost::shared_ptr<CPVRClient> client;
